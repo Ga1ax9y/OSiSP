74e6539d7d8725e90a00e108772e1f754211cb9f
Ga1ax9y <112807969+Ga1ax9y@users.noreply.github.com>
2024-12-11 21:26:25 +0300

    Add files via upload

---
+++ Osisp_lab5_client.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+const int PORT = 8080;
+const int BUFFER_SIZE = 512;
+
+int main() {
+    WSADATA wsaData;
+    SOCKET CSocket;
+    sockaddr_in SAddr;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Error: problem with WinSock\n";
+        return 1;
+    }
+
+    CSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (CSocket == INVALID_SOCKET) {
+        std::cerr << "Error: Problem with socket creation\n";
+        WSACleanup();
+        return 1;
+    }
+
+    SAddr.sin_family = AF_INET;
+    SAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &SAddr.sin_addr);
+
+    if (connect(CSocket, (sockaddr*)&SAddr, sizeof(SAddr)) == SOCKET_ERROR) {
+        std::cerr << "Error: Connection failed\n";
+        closesocket(CSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Connected to the server\n";
+
+    while (true) {
+        std::cout << "\nCommands:\n";
+        std::cout << "1. Send a message to the server\n";
+        std::cout << "2. Exit\n";
+        std::cout << "Your choice: ";
+
+        int choice;
+        std::cin >> choice;
+        std::cin.ignore();
+
+        if (choice == 2) {
+            std::string exitMessage = "exit";
+            send(CSocket, exitMessage.c_str(), exitMessage.length(), 0);
+            std::cout << "Disconnected...\n";
+            break;
+        }
+        else if (choice == 1) {
+            std::cout << "Your message: ";
+            std::string message;
+
+            std::getline(std::cin, message);
+            if (message.length() == 0) {
+                std::cout << "Error";
+            }
+            send(CSocket, message.c_str(), message.length(), 0);
+
+            char buff[BUFFER_SIZE] = { 0 };
+            int bytes = recv(CSocket, buff, BUFFER_SIZE, 0);
+            if (bytes > 0) {
+                std::cout << "Response from the server: " << buff << "\n";
+            }
+            else {
+                std::cout << "Error: problem with receiving response from server\n";
+            }
+        }
+        else {
+            std::cout << "Error: Incorreact choice. Try again.\n";
+        }
+    }
+
+    closesocket(CSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ Osisp_lab5_server.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <map>
+#pragma comment(lib, "ws2_32.lib")
+
+const int PORT = 8080;
+const int BUFFER_SIZE = 512;
+
+std::string textToMorse(const std::string& text) {
+    static const std::map<char, std::string> morseCode = {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+        {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+        {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+        {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+        {'Z', "--.."}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+        {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."},
+        {'8', "---.."}, {'9', "----."}, {'0', "-----"}
+    };
+
+    std::string result;
+    for (char c : text) {
+        c = toupper(c);
+        if (morseCode.find(c) != morseCode.end()) {
+            result += morseCode.at(c) + " ";
+        }
+        else if (c == ' ') {
+            result += "/ ";
+        }
+    }
+    return result;
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET SSocket, CSocket;
+    sockaddr_in SAddr, CAddr;
+    int CAddrSize = sizeof(CAddr);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Error: WSAStartup failed\n";
+        return 1;
+    }
+
+    SSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (SSocket == INVALID_SOCKET) {
+        std::cerr << "Error: Invalid socket\n";
+        WSACleanup();
+        return 1;
+    }
+
+    SAddr.sin_addr.s_addr = INADDR_ANY;
+    SAddr.sin_port = htons(PORT);
+    SAddr.sin_family = AF_INET;
+
+    if (bind(SSocket, (sockaddr*)&SAddr, sizeof(SAddr)) == SOCKET_ERROR) {
+        std::cerr << "Error: Problem with bind\n";
+        closesocket(SSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(SSocket, 5) == SOCKET_ERROR) {
+        std::cerr << "Error: Problem with socktet\n";
+        closesocket(SSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server works on port: " << PORT << "...\n";
+
+    while (true) {
+        CSocket = accept(SSocket, (sockaddr*)&CAddr, &CAddrSize);
+        if (CSocket == INVALID_SOCKET) {
+            std::cerr << "Error: Accept failed\n";
+            continue;
+        }
+
+        std::cout << "Client connected\n";
+
+        while (true) {
+            char buff[BUFFER_SIZE] = { 0 };
+            int bytesReceived = recv(CSocket, buff, BUFFER_SIZE, 0);
+            if (bytesReceived <= 0) {
+                std::cout << "Client disconnected\n";
+                break;
+            }
+
+            std::string request(buff);
+            if (request == "exit") {
+                std::cout << "Client disconnected\n";
+                break;
+            }
+
+            std::string response = textToMorse(request);
+            send(CSocket, response.c_str(), response.length(), 0);
+        }
+
+        closesocket(CSocket);
+    }
+
+    closesocket(SSocket);
+    WSACleanup();
+    return 0;
+}
